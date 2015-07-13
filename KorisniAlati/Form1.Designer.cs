6fb06c6f02966c31c2e92995821aac76c19ffa4d
Владимир Миливојевић <milivoyevich@yahoo.com>
2015-07-13 03:10:25 +0200

    prva verzija

---
+++ KorisniAlati/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KorisniAlati
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKonekcija = new System.Windows.Forms.TextBox();
+            this.btnKonekt = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtKonekcija
+            // 
+            this.txtKonekcija.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtKonekcija.Location = new System.Drawing.Point(38, 52);
+            this.txtKonekcija.Name = "txtKonekcija";
+            this.txtKonekcija.Size = new System.Drawing.Size(399, 21);
+            this.txtKonekcija.TabIndex = 0;
+            // 
+            // btnKonekt
+            // 
+            this.btnKonekt.Location = new System.Drawing.Point(494, 52);
+            this.btnKonekt.Name = "btnKonekt";
+            this.btnKonekt.Size = new System.Drawing.Size(75, 23);
+            this.btnKonekt.TabIndex = 1;
+            this.btnKonekt.Text = "Повежи се";
+            this.btnKonekt.UseVisualStyleBackColor = true;
+            this.btnKonekt.Click += new System.EventHandler(this.btnKonekt_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(43, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(269, 102);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(299, 221);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 371);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnKonekt);
+            this.Controls.Add(this.txtKonekcija);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "POCO - LOKO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKonekcija;
+        private System.Windows.Forms.Button btnKonekt;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ KorisniAlati/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KorisniAlati
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string initKonekt = @"Data Source=.\sqlexpress;Initial Catalog=NORTHWND;Integrated Security=True";
+        SqlConnection konekcija = new SqlConnection();
+        SqlCommand komanda=new SqlCommand(@"SELECT * FROM information_schema.tables where TABLE_TYPE='BASE TABLE'");
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable tabela, ttabla;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtKonekcija.Text = initKonekt;
+        }
+
+        private void btnKonekt_Click(object sender, EventArgs e)
+        {
+            konekcija.ConnectionString = txtKonekcija.Text;
+            komanda.Connection = konekcija;
+            adapter.SelectCommand = komanda;
+            tabela = new DataTable();
+            adapter.Fill(tabela);
+            listBox1.Items.Clear();
+            listBox1.DataSource = tabela;
+            listBox1.DisplayMember = "TABLE_NAME";
+            foreach (DataRow red in tabela.Rows)
+            {
+                ttabla = new DataTable();
+                komanda.CommandText = "SELECT TOP 1 * FROM ["+red["TABLE_NAME"].ToString()+"]";            
+                richTextBox1.AppendText("public class "+red["TABLE_NAME"].ToString()+"\n{\n");
+                adapter.Fill(ttabla);
+                foreach(DataColumn kolona in ttabla.Columns)
+                {
+                    richTextBox1.AppendText("public virtual "+ kolona.DataType.Name+" "+ kolona.ColumnName+ " {get; set; }\n");
+                }
+                richTextBox1.AppendText("}\n");
+                
+            }
+        }
+    }
+}
