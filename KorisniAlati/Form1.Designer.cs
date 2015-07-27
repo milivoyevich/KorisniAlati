7a0eb9e58538530d1c7bce567efe1bef713d50e2
Владимир Миливојевић <milivoyevich@yahoo.com>
2015-07-27 21:52:11 +0200

    ppp

--- KorisniAlati/Form1.Designer.cs
+++ KorisniAlati/Form1.Designer.cs
@@ -28,23 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtKonekcija = new System.Windows.Forms.TextBox();
-            this.btnKonekt = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NORTHWNDDataSet = new KorisniAlati.NORTHWNDDataSet();
+            this.tpPOCO = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkbView = new System.Windows.Forms.CheckBox();
+            this.chkbProc = new System.Windows.Forms.CheckBox();
+            this.chkbTable = new System.Windows.Forms.CheckBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnPoco = new System.Windows.Forms.Button();
+            this.txtKonekcija = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnKonekt = new System.Windows.Forms.Button();
+            this.tpXML = new System.Windows.Forms.TabPage();
+            this.btnXML = new System.Windows.Forms.Button();
+            this.rbtxtXML = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            ((System.ComponentModel.ISupportInitialize)(this.NORTHWNDDataSet)).BeginInit();
+            this.tpPOCO.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tpXML.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // NORTHWNDDataSet
+            // 
+            this.NORTHWNDDataSet.DataSetName = "NORTHWNDDataSet";
+            this.NORTHWNDDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tpPOCO
+            // 
+            this.tpPOCO.BackColor = System.Drawing.Color.BurlyWood;
+            this.tpPOCO.Controls.Add(this.groupBox1);
+            this.tpPOCO.Controls.Add(this.listBox1);
+            this.tpPOCO.Controls.Add(this.btnPoco);
+            this.tpPOCO.Controls.Add(this.txtKonekcija);
+            this.tpPOCO.Controls.Add(this.richTextBox1);
+            this.tpPOCO.Controls.Add(this.btnKonekt);
+            this.tpPOCO.Location = new System.Drawing.Point(4, 22);
+            this.tpPOCO.Name = "tpPOCO";
+            this.tpPOCO.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPOCO.Size = new System.Drawing.Size(603, 394);
+            this.tpPOCO.TabIndex = 0;
+            this.tpPOCO.Text = "POCO - LOKO";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkbView);
+            this.groupBox1.Controls.Add(this.chkbProc);
+            this.groupBox1.Controls.Add(this.chkbTable);
+            this.groupBox1.Location = new System.Drawing.Point(39, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 56);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Објекти";
+            // 
+            // chkbView
+            // 
+            this.chkbView.AutoSize = true;
+            this.chkbView.Location = new System.Drawing.Point(100, 24);
+            this.chkbView.Name = "chkbView";
+            this.chkbView.Size = new System.Drawing.Size(69, 17);
+            this.chkbView.TabIndex = 2;
+            this.chkbView.Text = "Погледи";
+            this.chkbView.UseVisualStyleBackColor = true;
+            // 
+            // chkbProc
+            // 
+            this.chkbProc.AutoSize = true;
+            this.chkbProc.Enabled = false;
+            this.chkbProc.Location = new System.Drawing.Point(186, 24);
+            this.chkbProc.Name = "chkbProc";
+            this.chkbProc.Size = new System.Drawing.Size(82, 17);
+            this.chkbProc.TabIndex = 1;
+            this.chkbProc.Text = "Процедуре";
+            this.chkbProc.UseVisualStyleBackColor = true;
+            // 
+            // chkbTable
+            // 
+            this.chkbTable.AutoSize = true;
+            this.chkbTable.Checked = true;
+            this.chkbTable.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkbTable.Location = new System.Drawing.Point(23, 24);
+            this.chkbTable.Name = "chkbTable";
+            this.chkbTable.Size = new System.Drawing.Size(62, 17);
+            this.chkbTable.TabIndex = 0;
+            this.chkbTable.Text = "Табеле";
+            this.chkbTable.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(36, 122);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnPoco
+            // 
+            this.btnPoco.Location = new System.Drawing.Point(449, 349);
+            this.btnPoco.Name = "btnPoco";
+            this.btnPoco.Size = new System.Drawing.Size(75, 23);
+            this.btnPoco.TabIndex = 4;
+            this.btnPoco.Text = "POCO";
+            this.btnPoco.UseVisualStyleBackColor = true;
+            this.btnPoco.Click += new System.EventHandler(this.btnPoco_Click);
             // 
             // txtKonekcija
             // 
             this.txtKonekcija.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtKonekcija.Location = new System.Drawing.Point(38, 52);
+            this.txtKonekcija.Location = new System.Drawing.Point(27, 20);
             this.txtKonekcija.Name = "txtKonekcija";
             this.txtKonekcija.Size = new System.Drawing.Size(399, 21);
             this.txtKonekcija.TabIndex = 0;
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(225, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(299, 221);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
             // btnKonekt
             // 
-            this.btnKonekt.Location = new System.Drawing.Point(494, 52);
+            this.btnKonekt.Location = new System.Drawing.Point(445, 18);
             this.btnKonekt.Name = "btnKonekt";
             this.btnKonekt.Size = new System.Drawing.Size(75, 23);
             this.btnKonekt.TabIndex = 1;
@@ -52,47 +161,87 @@
             this.btnKonekt.UseVisualStyleBackColor = true;
             this.btnKonekt.Click += new System.EventHandler(this.btnKonekt_Click);
             // 
-            // listBox1
+            // tpXML
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(43, 99);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(159, 225);
-            this.listBox1.TabIndex = 2;
+            this.tpXML.BackColor = System.Drawing.Color.LightSalmon;
+            this.tpXML.Controls.Add(this.btnXML);
+            this.tpXML.Controls.Add(this.rbtxtXML);
+            this.tpXML.Location = new System.Drawing.Point(4, 22);
+            this.tpXML.Name = "tpXML";
+            this.tpXML.Padding = new System.Windows.Forms.Padding(3);
+            this.tpXML.Size = new System.Drawing.Size(603, 394);
+            this.tpXML.TabIndex = 1;
+            this.tpXML.Text = "toXML";
             // 
-            // richTextBox1
+            // btnXML
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(269, 102);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(299, 221);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
+            this.btnXML.Location = new System.Drawing.Point(30, 53);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(75, 23);
+            this.btnXML.TabIndex = 1;
+            this.btnXML.Text = "to XML";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+            // 
+            // rbtxtXML
+            // 
+            this.rbtxtXML.Location = new System.Drawing.Point(208, 33);
+            this.rbtxtXML.Name = "rbtxtXML";
+            this.rbtxtXML.Size = new System.Drawing.Size(338, 323);
+            this.rbtxtXML.TabIndex = 0;
+            this.rbtxtXML.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpPOCO);
+            this.tabControl1.Controls.Add(this.tpXML);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(611, 420);
+            this.tabControl1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 371);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.btnKonekt);
-            this.Controls.Add(this.txtKonekcija);
+            this.ClientSize = new System.Drawing.Size(611, 420);
+            this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "POCO - LOKO";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NORTHWNDDataSet)).EndInit();
+            this.tpPOCO.ResumeLayout(false);
+            this.tpPOCO.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tpXML.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtKonekcija;
-        private System.Windows.Forms.Button btnKonekt;
+        private NORTHWNDDataSet NORTHWNDDataSet;
+        private System.Windows.Forms.TabPage tpPOCO;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkbView;
+        private System.Windows.Forms.CheckBox chkbProc;
+        private System.Windows.Forms.CheckBox chkbTable;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnPoco;
+        private System.Windows.Forms.TextBox txtKonekcija;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnKonekt;
+        private System.Windows.Forms.TabPage tpXML;
+        private System.Windows.Forms.Button btnXML;
+        private System.Windows.Forms.RichTextBox rbtxtXML;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
--- KorisniAlati/Form1.cs
+++ KorisniAlati/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Reflection;
 
 namespace KorisniAlati
 {
@@ -20,7 +24,7 @@ namespace KorisniAlati
         SqlConnection konekcija = new SqlConnection();
         SqlCommand komanda=new SqlCommand(@"SELECT * FROM information_schema.tables where TABLE_TYPE='BASE TABLE'");
         SqlDataAdapter adapter = new SqlDataAdapter();
-        DataTable tabela, ttabla;
+        DataTable tabela, ttabla, tempt;
         private void Form1_Load(object sender, EventArgs e)
         {
             txtKonekcija.Text = initKonekt;
@@ -28,27 +32,204 @@ namespace KorisniAlati
 
         private void btnKonekt_Click(object sender, EventArgs e)
         {
+            komanda.CommandText = "";
+            if (chkbTable.Checked) komanda.CommandText = @"SELECT 'table' as TIP, TABLE_NAME FROM information_schema.tables where TABLE_TYPE='BASE TABLE'";
+            if (chkbView.Checked)
+            {
+                if (komanda.CommandText.Length > 0) komanda.CommandText += " UNION ";
+                komanda.CommandText += "SELECT 'view' as TIP, TABLE_NAME FROM information_schema.views";
+            }
+            if (chkbProc.Checked)
+            {
+                if (komanda.CommandText.Length > 0) komanda.CommandText += " UNION ";
+                komanda.CommandText += "SELECT 'proc' as TIP, ROUTINE_NAME as TABLE_NAME FROM information_schema.routines";
+            }
+            if(komanda.CommandText=="")
+            {
+                MessageBox.Show("Нисте изабрали ниједну врсту објеката!", "Упозорење"); return;
+            }
             konekcija.ConnectionString = txtKonekcija.Text;
             komanda.Connection = konekcija;
             adapter.SelectCommand = komanda;
             tabela = new DataTable();
             adapter.Fill(tabela);
-            listBox1.Items.Clear();
+            listBox1.DataSource = null;
             listBox1.DataSource = tabela;
             listBox1.DisplayMember = "TABLE_NAME";
-            foreach (DataRow red in tabela.Rows)
-            {
-                ttabla = new DataTable();
-                komanda.CommandText = "SELECT TOP 1 * FROM ["+red["TABLE_NAME"].ToString()+"]";            
-                richTextBox1.AppendText("public class "+red["TABLE_NAME"].ToString()+"\n{\n");
-                adapter.Fill(ttabla);
-                foreach(DataColumn kolona in ttabla.Columns)
-                {
-                    richTextBox1.AppendText("public virtual "+ kolona.DataType.Name+" "+ kolona.ColumnName+ " {get; set; }\n");
-                }
-                richTextBox1.AppendText("}\n");
+            //foreach (DataRow red in tabela.Rows)
+            //{
+            //    ttabla = new DataTable();
+            //    komanda.CommandText = "SELECT TOP 1 * FROM ["+red["TABLE_NAME"].ToString()+"]";            
+            //    richTextBox1.AppendText("public class "+red["TABLE_NAME"].ToString()+"\n{\n");
+            //    adapter.Fill(ttabla);
+            //    foreach(DataColumn kolona in ttabla.Columns)
+            //    {
+            //        richTextBox1.AppendText("public virtual "+ kolona.DataType.Name+" "+ kolona.ColumnName+ " {get; set; }\n");
+            //    }
+            //    richTextBox1.AppendText("}\n");
                 
+            //}
+        }
+        private void btnPoco_Click(object sender, EventArgs e)
+        {
+            bool moze_null = false;
+            if (listBox1.SelectedIndex > -1)
+            {
+                ttabla = new DataTable(); tempt = new DataTable();
+                komanda.CommandText = "SELECT TOP 1 * FROM [" + listBox1.Text + "]";
+                richTextBox1.AppendText("[Serializable]\n");
+                richTextBox1.AppendText("public class " + listBox1.Text + "\n{\n");
+                adapter.Fill(ttabla);
+                komanda.CommandText = "Select name, type_name(user_type_id) as Type, is_nullable as Nullable from sys.all_columns" +
+                " where object_id = OBJECT_ID('" + listBox1.Text + "')"; //
+                adapter.Fill(tempt);
+                richTextBox1.AppendText("#region Konstruktor\n\n");
+                richTextBox1.AppendText("public " + listBox1.Text + "():base()\n{\n}\n");
+                richTextBox1.AppendText("\n#endregion\n\n");
+                richTextBox1.AppendText("#region Chlanovi\n\n");
+                foreach (DataColumn kolona in ttabla.Columns)
+                {
+                  DataRow[] redovi= tempt.Select("name='" + kolona.ColumnName + "'");
+                  if (redovi != null && redovi.Length == 1) moze_null = (bool)redovi[0]["Nullable"];
+                    richTextBox1.AppendText("private " + kolona.DataType.Name + ((moze_null && kolona.DataType.Name.ToLower() != "string") ? "?" : "") + " _" + kolona.ColumnName + ";\n");
+                }
+                richTextBox1.AppendText("\n#endregion\n\n");
+                richTextBox1.AppendText("#region Osobine\n\n");
+                foreach (DataColumn kolona in ttabla.Columns)
+                {
+                    DataRow[] redovi = tempt.Select("name='" + kolona.ColumnName + "'");
+                    if (redovi != null && redovi.Length == 1) moze_null = (bool)redovi[0]["Nullable"];
+                    richTextBox1.AppendText("public virtual " + kolona.DataType.Name + 
+                        ((moze_null && kolona.DataType.Name.ToLower() != "string") ? "?" : "") + 
+                        " " + kolona.ColumnName + " {\n      get { return _" + kolona.ColumnName + 
+                        " ; }\n      set \n      {\n         if(_" + kolona.ColumnName + " != value)\n         {\n             _" + 
+                        kolona.ColumnName + "=value;\n            SetDirty();\n         }\n      }\n");
+                }
+                richTextBox1.AppendText("\n#endregion\n\n");
+                richTextBox1.AppendText("#region nadjacane Osobine\n\n");
+                richTextBox1.AppendText("public override bool IsDirty\n{\n      get { return this.isDirty; }\n}\n");
+                richTextBox1.AppendText("\n#endregion\n\n");
+                richTextBox1.AppendText("#region nadjacane Metode\n\n");
+                richTextBox1.AppendText("public override void SetClean()\n{\n      this.isDirty = false;\n}\n");
+                richTextBox1.AppendText("public override void SetOld()\n{\n      this.isNew = false;\n}\n");
+                richTextBox1.AppendText("protected override object Copy(object src, object dst)\n{\n         "+
+                 listBox1.Text + " from = (" + listBox1.Text + ")src;\n         "
+             + listBox1.Text + " to = (" + listBox1.Text + ")dst;\n\n"+
+            "         to.id = from.id;\n         to.sifra = from.sifra;\n         to.rowVersion = from.rowVersion;\n\n"+
+            "         to.isNew = from.isNew;\n         to.isDirty = from.isDirty;\n         to.isDeleted = from.isDeleted;\n\n");
+                foreach (DataColumn kolona in ttabla.Columns)
+                {
+                    richTextBox1.AppendText("         to._"+kolona.ColumnName+" = from._"+kolona.ColumnName+";\n");
+                }
+                richTextBox1.AppendText("\n         return dst;\n}\n");
+                richTextBox1.AppendText("public override object Clone()\n      {\n         "+
+                listBox1.Text + " clone = (" + listBox1.Text + ")Copy(this, new " + listBox1.Text + "());" +
+                "\n         return clone;\n      }\n");
+                richTextBox1.AppendText("public override bool Validate(out string message)\n{\n      "+
+                "bool validno = true;\n      message = string.Empty;\n      \n      return validno;\n}\n");
+                richTextBox1.AppendText("\n#endregion\n\n");
+                richTextBox1.AppendText("#region Jednachenja\n\n");
+                richTextBox1.AppendText("public override int GetHashCode()\n{\n      return base.GetHashCode();\n}\n");
+                richTextBox1.AppendText("public override bool Equals(object obj)\n{\n      "+
+                    "if ((obj == null) || (GetType() != obj.GetType()))\n      {\n         return false;\n      }\n"+
+                    "      " + listBox1.Text + " ps = (" + listBox1.Text + ")obj;\n      "+
+                    "return this." + listBox1.Text + " == ps." + listBox1.Text + ";\n}\n");
+                richTextBox1.AppendText("public static bool operator ==(" + listBox1.Text + " po1, " + listBox1.Text + " po2)\n" +
+                    "{\n      if ((ReferenceEquals(po1, null)))\n      {\n         return ReferenceEquals(po2, null);\n"+
+                    "      }\n      else\n      {\n         return po1.Equals(po2);\n      }\n}\n");
+                richTextBox1.AppendText("public static bool operator !=(JciHdr po1, JciHdr po2)\n"+
+                    "{\n      return !(po1 == po2);\n}\n");
+                richTextBox1.AppendText("\n#endregion\n\n");
+                richTextBox1.AppendText("}\n");
             }
         }
+
+        private object Popuni_objekat(object objekat)
+        {
+            Type myType = objekat.GetType(); 
+            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+            foreach (PropertyInfo prop in props)
+            {
+               // MessageBox.Show(prop.PropertyType.Name); 
+               switch (prop.PropertyType.Name)
+               {
+                   case "String": prop.SetValue(objekat, "", null); break;
+                   case "DateTime": prop.SetValue(objekat, DateTime.MinValue, null); break;
+                   case "Int64":
+                   case "Int32": //prop.SetValue(objekat, null, null); break;
+                   case "Int16": break; //prop.SetValue(objekat, null, null);
+                   case "Decimal": prop.SetValue(objekat, 0m, null); break;
+                   case "Object":
+                       //Type TipO = (Type)prop.ReflectedType.GetElementType();
+                       //object objekat11 = Activator.CreateInstance(TipO);
+                       
+                       // prop.SetValue(objekat, Convert.ChangeType(objekat11, prop.ReflectedType.GetElementType()), null);
+                       //    Type myType11 = objekat11.GetType();
+                       //    IList<PropertyInfo> props11 = new List<PropertyInfo>(myType11.GetProperties());
+                       //    if (props11.Count > 0) Popuni_objekat(objekat11);
+                       break;
+                   case "Boolean": break;
+                   default: 
+                       Type Tip = (Type)prop.PropertyType;
+                       if (!prop.PropertyType.IsArray)
+                       {
+                           object objekat1 = Activator.CreateInstance(Tip);
+                           prop.SetValue(objekat, Convert.ChangeType(objekat1, prop.PropertyType), null);
+                           Type myType1 = objekat1.GetType();
+                           IList<PropertyInfo> props1 = new List<PropertyInfo>(myType1.GetProperties());
+                           if (props1.Count > 0) Popuni_objekat(objekat1);
+                       }
+                       else
+                       {
+                      //  MessageBox.Show(prop.PropertyType.GetElementType().Name,"Element");
+                        object objekat1 = Activator.CreateInstance(Tip, new object[] {1});
+                        prop.SetValue(objekat, Convert.ChangeType(objekat1, prop.PropertyType), null);
+                        Type myType1 = objekat1.GetType();
+                        IList<PropertyInfo> props1 = new List<PropertyInfo>(myType1.GetProperties());
+                        if (props1.Count > 0) Popuni_objekat(objekat1);
+                       }
+                       break;
+               }
+                
+            }
+            return objekat;
+        }
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            //XmlDocument Dokument = new XmlDocument();
+            CusDecMessage Dokument = new CusDecMessage();
+            Dokument = (CusDecMessage)Popuni_objekat(Dokument);
+            //Dokument.Header = new HeaderType();
+            //Dokument.Header.SenderDetails = new HeaderTypeSenderDetails();
+            //Dokument.Header.SenderDetails.IDAuthentication = new HeaderTypeSenderDetailsIDAuthentication();
+            //Dokument.Header.SenderDetails.IDAuthentication.SenderID = "";
+            //Dokument.Header.SenderDetails.Topic = "";
+            //Dokument.Header.SenderDetails.Version = "";
+            //Dokument.Body = new CusDecMessageBody();
+            //Dokument.Body.MessageFunction = new CusDecMessageBodyMessageFunction[1];
+            //Dokument.Body.MessageFunction[0] = new CusDecMessageBodyMessageFunction();
+            //Dokument.Body.MessageHeader = new CusDecMessageBodyMessageHeader();
+
+            //Type myType = Dokument.GetType();
+            //IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+            //foreach (PropertyInfo prop in props)
+            //{
+            //    Type Tip = (Type)prop.PropertyType;
+            //    object objekat = Activator.CreateInstance(Tip);
+            //    prop.SetValue(Dokument, Convert.ChangeType(objekat, prop.PropertyType), null);             
+            //}
+            
+
+            XmlSerializer serijal = new XmlSerializer(typeof(CusDecMessage));          
+             StringBuilder sb = new StringBuilder();
+             XmlWriterSettings postavke = new XmlWriterSettings();
+             postavke.Indent = true; postavke.OmitXmlDeclaration = false;
+          
+            XmlWriter writer = XmlWriter.Create(sb,postavke);
+            var nsSer=new XmlSerializerNamespaces(); nsSer.Add (string.Empty,string.Empty);
+            serijal.Serialize(writer, Dokument,nsSer);
+            rbtxtXML.AppendText(sb.ToString());
+        }
+        
     }
 }
